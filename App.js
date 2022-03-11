3d597497eefe541b926c2a9bdde508678e0467fc
edison16 <edison@founderslair.com>
2022-03-11 22:14:17 +0800

    added acc and gyro sensors

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AccSensors from './components/AccSensors';
+import GyroSensors from './components/GyroSensors';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>created by Daniel Edison</Text>
+      <AccSensors />
+      <GyroSensors />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +21,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontWeight: 'bold',
+  }
 });
